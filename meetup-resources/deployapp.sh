2f72964464ffb529c8255c8c6051076d4d21c378
depauna <natan.depauw@flowfactor.be>
2018-10-12 12:16:41 +0200

    deployapp.sh update

--- meetup-resources/deployapp.sh
+++ meetup-resources/deployapp.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-docker_hub_username = $1
-img_name = $2
-img_tag = $3
-firstName = $4
-lastName = $5
+docker_hub_username=$1
+img_name=$2
+img_tag=$3
+firstName=$4
+lastName=$5
 
-helm upgrade --tls --tls-ca-cert ~/.helm/ca.pem --tls-cert ~/.helm/cert.pem --tls-key ~/.helm/key.pem --wait --install -f /var/jenkins_home/workspace/book-store/meetup-resources/meetup-chart-book-store/book-store.values.yaml --namespace jenkins book-store-stable /var/jenkins_home/workspace/book-store/meetup-resources/meetup-chart-book-store/bookstore-app
+helm upgrade --tls --tls-ca-cert ~/.helm/ca.pem --tls-cert ~/.helm/cert.pem --tls-key ~/.helm/key.pem --wait --install -f /var/jenkins_home/workspace/book-store/meetup-resources/meetup-chart-book-store/book-store.values.yaml --namespace jenkins --set img.tag=$img_tag $4$5-book-store-stable $3 /var/jenkins_home/workspace/book-store/meetup-resources/meetup-chart-book-store/bookstore-app
